d3e9e88e759d794b37cb0196abd6eaafd5de92fb
con0fav <connor.favero@gmail.com>
2020-11-28 22:02:02 -0800

    boilerplate questions array with notes

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+// array of questions for user
+const readmeQuestions = [
+      {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: 'Describe your project',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Table of contents',
+        name: 'contents',
+      },
+      {
+        type: 'input',
+        message: 'How do you install this?',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'How do you use this?',
+        name: 'usage',
+      },
+      // no idea what this section means yet
+      {
+        type: 'input',
+        message: 'What are the tests? or How do you test this?',
+        name: 'tests',
+      },
+      // make a list of options for license
+      {
+        type: 'input',
+        message: 'What is the license?',
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+      }
 ];
 
 // function to write README file
